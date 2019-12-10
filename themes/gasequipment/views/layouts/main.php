6696194382572f9a0244ebc63cd9a02d0d1e4457
toropovpavel1991 <toropov_pv@bk.ru>
2019-12-10 17:04:21 +0700

    template

--- themes/gasequipment/views/layouts/main.php
+++ themes/gasequipment/views/layouts/main.php
@@ -69,83 +69,25 @@
     <footer class="section-full">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-footer-widget">
-                        <h6 class="text-white text-uppercase mb-20">About Agency</h6>
-                        <p>The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point </p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-footer-widget">
-                        <h6 class="text-white text-uppercase mb-20">Navigation Links</h6>
-                        <?php if (Yii::app()->hasModule('menu')): ?>
-                            <?php $this->widget(
-                                'application.modules.menu.widgets.MenuWidget',
-                                [
-                                    'name' => 'top-menu',
-                                    'view' => 'footer'
-                                ]);?>
-                        <?php endif; ?>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-footer-widget">
-                        <h6 class="text-white text-uppercase mb-20">Newsletter</h6>
-                        <p>For business professionals caught between high OEM price and mediocre print and graphic output,</p>
-                        <div id="mc_embed_signup">
-                            <form target="_blank" novalidate action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&id=92a4423d01" method="get" class="subscription relative d-flex justify-content-center">
-                                <input type="email" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required>
-                                <div style="position: absolute; left: -5000px;">
-                                    <input type="text" name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="">
-                                </div>
-                                <button type="submit" class="newsletter-btn" name="subscribe"><span class="lnr lnr-location"></span></button>
-                                <div class="info"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-footer-widget">
-                        <h6 class="text-white text-uppercase mb-20">Instafeed</h6>
-                        <ul class="instafeed d-flex flex-wrap">
-                            <li><img src="img/i1.jpg" alt=""></li>
-                            <li><img src="img/i2.jpg" alt=""></li>
-                            <li><img src="img/i3.jpg" alt=""></li>
-                            <li><img src="img/i4.jpg" alt=""></li>
-                            <li><img src="img/i5.jpg" alt=""></li>
-                            <li><img src="img/i6.jpg" alt=""></li>
-                            <li><img src="img/i7.jpg" alt=""></li>
-                            <li><img src="img/i8.jpg" alt=""></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom d-flex justify-content-between align-items-center">
-                <p class="footer-text m-0">Copyright &copy; 2017  |  All rights reserved to <a href="#">Datarc inc.</a> Designed by <a href="https://colorlib.com/wp">Colorlib</a>.</p>
-                <div class="footer-social d-flex align-items-center">
-                    <a href="#"><i class="fa fa-facebook"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
-                    <a href="#"><i class="fa fa-dribbble"></i></a>
-                    <a href="#"><i class="fa fa-behance"></i></a>
-                </div>
+                <? $this->widget('application.modules.zendsearch.widgets.SearchBlockWidget'); ?>
+                <nav class="top-menu">
+                <?php if (Yii::app()->hasModule('menu')): ?>
+                    <?php $this->widget('application.modules.menu.widgets.MenuWidget', ['name' => 'top-menu']); ?>
+                <?php endif;?>
+                </nav>
             </div>
         </div>
     </footer>
     <?php \yupe\components\TemplateEvent::fire(ShopThemeEvents::BODY_END);?>
     <div class='notifications top-right' id="notifications"></div>
     <?php
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/index.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/jquery.collapse.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/jquery.collapse_storage.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/jquery.fancybox.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/overlay.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/product-gallery.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/slick.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/slick.min.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/tabs.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/toggle.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/libs/select2/select2.min.js', CClientScript::POS_END);
-    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/store.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/vendor/jquery-2.2.4.min.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/vendor/bootstrap.min.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/jquery.ajaxchimp.min.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/jquery.sticky.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/main.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/mixitup.min.js', CClientScript::POS_END);
+    Yii::app()->getClientScript()->registerScriptFile($this->mainAssets . '/js/owl.carousel.min.js', CClientScript::POS_END);
     ?>
 </body>
 </html>
--- themes/gasequipment/views/site/index.php
+++ themes/gasequipment/views/site/index.php
@@ -1,4 +1,160 @@
 <?php $this->title = Yii::app()->getModule('yupe')->siteName; ?>
+<div class="content body-content">
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="img-fluid" src="/images/kotel.jpg" alt="image">
+        </div>
+        <div class="carousel-item">
+          <img class="img-fluid" src="/images/kotel.jpg" alt="image">
+        </div>
+        <div class="carousel-item">
+           <img class="img-fluid" src="/images/kotel.jpg" alt="image">
+        </div>
+      </div>
+      <!-- Элементы управления -->
+      <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Предыдущий</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Следующий</span>
+      </a>
+    </div>
+    <section id="protfolio" class="section-full">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="product-area-title text-center">
+                        <h2 class="h1">Продукция</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="filter-content" class="row no-gutters mt-70">
+            <div class="mix category-1 col-lg-3 col-md-4 col-sm-6 single-filter-content content-1" data-myorder="1">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Баллоны отчественные</a>
+                </div>
+            </div>
+            <div class="mix category-1 category-5 category-3 category-5 col-lg-3 col-md-4 col-sm-6 single-filter-content content-2" data-myorder="2">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Итальянское оборудование и комплектующие</a>
+                </div>
+            </div>
+            <div class="mix category-1 category-5 col-lg-3 col-md-4 col-sm-6 single-filter-content content-3" data-myorder="3">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Комплектующие ВПРЫСК (н)</a>
+                </div>
+            </div>
+            <div class="mix category-2 category-3 category-6 col-lg-3 col-md-4 col-sm-6 single-filter-content content-4" data-myorder="4">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Шланги</a>
+                </div>
+            </div>
+            <div class="mix category-1 category-4 category-5 col-lg-3 col-md-4 col-sm-6 single-filter-content content-5" data-myorder="5">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Электроника Резол</a>
+                </div>
+            </div>
+            <div class="mix category-1 category-3 category-5 category-6 col-lg-3 col-md-4 col-sm-6 single-filter-content content-6" data-myorder="6">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Смесители</a>
+                </div>
+            </div>
+            <div class="mix category-2 category-4 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-7" data-myorder="7">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Сервисное оборудование</a>
+                </div>
+            </div>
+            <div class="mix category-2 category-6 category-5 col-lg-3 col-md-4 col-sm-6 single-filter-content content-8" data-myorder="8">
+                <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
+                    <a href="">Ремкомплекты и фильтры</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-full studio-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="studio-content">
+                        <h2 class="h1 text-white text-uppercase mb-20">О компании</h2>
+                        <p class="text-white mb-30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-full">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="product-area-title text-center">
+                        <h2 class="h1">Акции</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="single-pricing-table">
+                        <div class="top">
+                            
+                        </div>
+
+                        <div class="bottom text-center">
+                            <ul class="feature text-center">
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aenean euismod bibendum laoreet. 
+                                Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. 
+                                Proin sodales pulvinar tempor.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-pricing-table">
+                        <div class="top">
+                            
+                        </div>
+
+                        <div class="bottom text-center">
+                            <ul class="feature text-center">
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aenean euismod bibendum laoreet. 
+                                Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. 
+                                Proin sodales pulvinar tempor.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-pricing-table">
+                        <div class="top">
+                            
+                        </div>
+
+                        <div class="bottom text-center">
+                            <ul class="feature text-center">
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aenean euismod bibendum laoreet. 
+                                Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. 
+                                Proin sodales pulvinar tempor.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <?/*
 <!-- Start Banner Area -->
         <section class="banner-area relative">
