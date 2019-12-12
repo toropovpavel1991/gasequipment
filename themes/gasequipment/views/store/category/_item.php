e10febbfd729278b457f43c155685ce98b0bab4f
Pavel Toropov <toropovpavel1991@gmail.com>
2019-12-12 17:29:57 +0700

    fix

--- themes/gasequipment/views/store/category/_item.php
+++ themes/gasequipment/views/store/category/_item.php
@@ -1,25 +1,3 @@
-<?php/*
-<div class="catalog__category-item">
-    <?php if ($data['icon']): ?>
-        <a href="<?= $data['url']; ?>">
-            <img src="<?= $data['icon']; ?>" alt="<?= $data['icon_alt']; ?>" title="<?= $data['icon_title']; ?>"/>
-        </a>
-    <?php endif; ?>
-    <a href="<?= $data['url']; ?>" class="catalog__category-item-title"><?= CHtml::encode($data['label']); ?></a>
-    <?php if ($data['items']): ?>
-        <ul>
-            <?php foreach ($data['items'] as $item): ?>
-                <li><a href="<?= $item['url'] ?>"><?= $item['label'] ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-</div>
-*/?>
-<?php/*
-<?echo '<pre>';?>
-<?print_r($data);?>
-<?echo '</pre>';?>
-*/?>
 <div class="mix col-lg-3 col-md-4 col-sm-6 single-filter-content content-1">
     <?php/*
     <div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
--- themes/gasequipment/views/store/producer/index.php
+++ themes/gasequipment/views/store/producer/index.php
@@ -38,34 +38,3 @@ $this->breadcrumbs = [
         ]); ?>
     </div>
 </section>
-<?/*
-<div class="main__title grid">
-    <h1 class="h2">
-        <?= Yii::t('StoreModule.store', 'Producers list'); ?>
-    </h1>
-</div>
-
-<div class="main__catalog grid">
-    <div class="cols">
-        <div class="col grid-module-12">
-            <?php $this->widget('zii.widgets.CListView', [
-                'dataProvider' => $brands,
-                'itemView' => '_item',
-                'template' => '{items} {pager}',
-                'itemsCssClass' => 'catalog__items',
-                'pagerCssClass' => 'catalog__pagination',
-                'pager' => [
-                    'header' => '',
-                    'prevPageLabel' => '<i class="fa fa-long-arrow-left"></i>',
-                    'nextPageLabel' => '<i class="fa fa-long-arrow-right"></i>',
-                    'firstPageLabel' => false,
-                    'lastPageLabel' => false,
-                    'htmlOptions' => [
-                        'class' => 'pagination'
-                    ]
-                ]
-            ]); ?>
-        </div>
-    </div>
-</div>
-*/?>
